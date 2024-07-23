3068ca6cdf08a9a9ca3b98cac1e2e13b1d501d79
Alana McKeel <97997865+alanam79@users.noreply.github.com>
2024-07-23 14:32:23 -0400

    Add files via upload

---
+++ Day 1/main.py
@@ -0,0 +1,10 @@
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+# print(len(input("What is your name? "))) this equals the same as below
+name = input("What is your name?")
+length = len(name)
+print(length)
